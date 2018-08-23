72ccb9e9865c515ec2d42f0a29b38ea2265359a3
ldnatcapgroup <ldn@capgroup.com>
2018-08-23 09:49:07 -0700

    add AAM

--- aws-java-simple-http-endpoint/src/main/java/com/serverless/Handler.java
+++ aws-java-simple-http-endpoint/src/main/java/com/serverless/Handler.java
@@ -23,41 +23,67 @@ public class Handler implements RequestHandler<Map<String, Object>, String> {
 
 	private static final Logger LOG = Logger.getLogger(Handler.class);
 	private static String accessToken = "OAuth 00D0x0000000QNK!ARcAQJLKTMkFzNFQYcy1T0GfJf14AXJrBua14ZsJPYRY2No7c5QXZC47FTcB6ciaHF3AN6AwhCtgt1VemriK6HD6L_5LBFRc";
-	private static String vAddress = "https://xn--capgroupfull-r19f.cs95.my.salesforce.com/services/data/v36.0/sobjects/Notification__c";
+	private static String sfdcUrl = "https://xn--capgroupfull-r19f.cs95.my.salesforce.com/services/data/v36.0/sobjects/Notification__c";
+	private static String aamUrl = "http://capitalgroup.demdex.net/event?d_uuid=48515798915548649800232462535070166586&d_sid=11940189&d_rtbd=json";
+	
+	private String invokeSFDC() throws IOException {
+		String json = "{\"Active__c\" : \"true\",\"Description__c\" : \"Test6\",\"Start_Date__c\" : \"2018-08-23\"}";
+		
+		URL url = new URL(sfdcUrl);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        
+        conn.setRequestProperty("Authorization", accessToken);
+        conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+        conn.setRequestProperty("Accept", "application/json; charset=UTF-8");
+        conn.setDoOutput(true);
+        
+        OutputStream os = conn.getOutputStream();
+        os.write(json.getBytes("UTF-8"));
+        os.close();
+        	       
+        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
 
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+		
+		return response.toString();
+	}
+
+	private String invokeAAM() throws IOException {
+		
+		URL url = new URL(aamUrl);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("GET");
+
+		int responseCode = conn.getResponseCode();
+		System.out.println("Response Code = " + responseCode);
+		BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+		System.out.println(response.toString());
+		return response.toString();		
+	}
+	
 	@Override
 	public String handleRequest(Map<String, Object> input, Context context) {
-		LOG.info("received: " + input);
-		HttpURLConnection conn;
-		String json = "{\"Active__c\" : \"true\",\"Description__c\" : \"Test5\",\"Start_Date__c\" : \"2018-08-23\"}";
 		
 		try {
 			
-	        URL url = new URL(vAddress);
-	        conn = (HttpURLConnection) url.openConnection();
-            
-	        conn.setRequestProperty("Authorization", accessToken);
-	        conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-	        conn.setRequestProperty("Accept", "application/json; charset=UTF-8");
-	        conn.setDoOutput(true);
-	        
-	        OutputStream os = conn.getOutputStream();
-	        os.write(json.getBytes("UTF-8"));
-	        os.close();
-	        	       
-	        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			String inputLine;
-			StringBuffer response = new StringBuffer();
-
-			while ((inputLine = in.readLine()) != null) {
-				response.append(inputLine);
-			}
-			in.close();
+			invokeAAM();
 			
-			return response.toString();
+	        invokeSFDC();
 	        
 	        // return String.valueOf(conn.getResponseCode()) +" : "+ conn.getResponseMessage();
-			// return "{id:'abc123', status:'new'}";
+			return "{id:'abc123', status:'new'}";
 
 	      } catch (IOException e) {
 
